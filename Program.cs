a34da35ddba76be7c20e438fd2c71773c0673333
ChristopherChiasson139 <Christopher139139@gmail.com>
2021-09-27 13:56:26 -0300

    3

--- Program.cs
+++ Program.cs
@@ -12,30 +12,58 @@ namespace methods_2
         static int score;
         static int health;
 
+        
         static void ShowHUD()
         {
+            Console.WriteLine("-------------");
             Console.WriteLine("Health = " + health);
+            Console.WriteLine("Score = " + score);
+            Console.WriteLine("-------------");
+        }
 
-
+        static void Pont(int pont)
+        {
+            score = score + pont;
         }
 
         static void TakeDamage(int damage)
         {
-            health = health - damage;
+            health = health + damage;
         }
         
         static void Main(string[] args)
         {
+            Console.WriteLine("Name: Mario");
 
-            Console.WriteLine("met2");
 
             score = 0;
-            health = 100;
+            health = 10;
 
             ShowHUD();
-            TakeDamage(10);
+            Console.ReadKey(true);
+            Console.WriteLine("Mario killed a Goomba");
+            Console.ReadKey(true);
+            Pont(10);
             ShowHUD();
-
+            Console.ReadKey(true);
+            Console.WriteLine("Mario killed a fat Goomba");
+            Console.ReadKey(true);
+            Pont(30);
+            ShowHUD();
+            Console.ReadKey(true);
+            Console.WriteLine("Mario was hit by a Koopa");
+            Console.ReadKey(true);
+            TakeDamage(-5);
+            Pont(-5);
+            ShowHUD();
+            Console.ReadKey(true);
+            Console.WriteLine("Mario eat some food");
+            Console.ReadKey(true);
+            Pont(3);
+            TakeDamage(3);
+            ShowHUD();
+            Console.ReadKey(true);
+            Console.WriteLine("Mario want to bed");
             Console.ReadKey(true);
         }
     }
