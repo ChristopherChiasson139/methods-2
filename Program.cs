ed09aba7e3156acc1f1369bb89c6eadcdb946a6a
ChristopherChiasson139 <Christopher139139@gmail.com>
2021-09-27 13:08:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace methods_2
 {
+    
     class Program
     {
+        static int score;
+        static int health;
+
+        static void ShowHUD()
+        {
+            Console.WriteLine("Health = " + health);
+
+
+        }
+
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+        
         static void Main(string[] args)
         {
+
+            Console.WriteLine("met2");
+
+            score = 0;
+            health = 100;
+
+            ShowHUD();
+            TakeDamage(5);
+            ShowHUD();
+
+            Console.ReadKey(true);
         }
     }
 }
